e91c41466e5e40ce7e8b60914ec669e0fe05e7d8
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-30 19:56:12 +0600

    add arrww

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,38 @@
 import { FaArrowCircleRight } from "react-icons/fa";
 const Home = () => {
+  const addTask = (e) => {
+    e.preventDefault();
+    const taskName = e.target.tName.value;
+    const taskDetails = e.target.details.value;
+
+    console.log(taskName, taskDetails);
+  };
+  //   fetch("", {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       title: "foo",
+  //       body: "bar",
+  //       userId: 1,
+  //     }),
+  //     headers: {
+  //       "Content-type": "application/json",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
   return (
     <div className="home-container">
       <h2>Add Your Task</h2>
-      <div className="home-info">
+
+      <form className="home-info" onSubmit={addTask}>
         <label>Your Task Name</label>
-        <input type="text" />
+        <input type="text" name="tName" />
         <label>Your Task Details</label>
-        <textarea type="text" />
-        <p className="arrow">
+        <textarea type="text" name="details" />
+        <button type="submit" className="arrow">
           <FaArrowCircleRight />
-        </p>
-      </div>
+        </button>
+      </form>
     </div>
   );
 };
