dd09f7ea71f94fb0d9a6a4f4a6fbed79fa60824c
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-30 14:17:31 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Pages/Shared/Header";
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="px-5">
+     
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#features">Completed Tasks</Nav.Link>
+              <Nav.Link href="#pricing"> To-Do</Nav.Link>
+              <Nav.Link href="#pricing"> Calendar</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
