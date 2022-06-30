040f1a072b8a08b8c28396607940c0754d29897a
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-30 22:57:01 +0600

    list info

--- src/Pages/Home/TaskList.js
+++ src/Pages/Home/TaskList.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState({});
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/allTask")
       .then((res) => res.json())
       .then((data) => setTasks(data));
   }, []);
-  return <div>Task length:{tasks.length}</div>;
+  return (
+    <div>
+      <div className="allTasks-container px-5">
+        {tasks.map((task) => (
+          <div className="all-task-info">
+            <p className="edit">
+              <FaEdit />
+            </p>
+            <div className="taskTitle">Name: {task.name}</div>
+            <hr />
+            <div className="taskDetails">Details: {task.details}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
